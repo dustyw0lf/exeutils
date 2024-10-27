00b080080449de099a9aebb35d87974f660263eb
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-28 00:08:18 +0200

    Make internal ELF submodule to better control public exports

--- src/elf.rs
+++ src/elf.rs
@@ -10,118 +10,122 @@
 //! Linux](https://www.muppetlabs.com/~breadbox/software/tiny/teensy.html)" and
 //! the code in <https://github.com/AjayBrahmakshatriya/minimal-elf/>.
 
-#![allow(non_camel_case_types)]
+mod elf_internal {
+    #![allow(non_camel_case_types)]
 
-use binary_layout::prelude::*;
+    use binary_layout::prelude::*;
 
-type Elf64_Addr = u64;
-type Elf64_Off = u64;
-type Elf64_Half = u16;
-type Elf64_Word = u32;
-// type Elf64_Sword = i32;
-type Elf64_Xword = u64;
-// type Elf64_Sxword = i64;
+    type Elf64_Addr = u64;
+    type Elf64_Off = u64;
+    type Elf64_Half = u16;
+    type Elf64_Word = u32;
+    // type Elf64_Sword = i32;
+    type Elf64_Xword = u64;
+    // type Elf64_Sxword = i64;
 
-binary_layout!(elf64_ident, LittleEndian, {
-    mag: [u8; 4],
-    class: u8,
-    data: u8,
-    version: u8,
-    os_abi: u8,
-    abi_version: u8,
-    pad: [u8; 7],
-});
+    binary_layout!(elf64_ident, LittleEndian, {
+        mag: [u8; 4],
+        class: u8,
+        data: u8,
+        version: u8,
+        os_abi: u8,
+        abi_version: u8,
+        pad: [u8; 7],
+    });
 
-fn set_ident<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: elf64_ident::View<S>) {
-    view.mag_mut()
-        .copy_from_slice(&[0x7f, 'E' as u8, 'L' as u8, 'F' as u8]);
-    view.class_mut().write(2); // class: ELFCLASS64
-    view.data_mut().write(1); // data encoding: ELFDATA2LSB
-    view.version_mut().write(1); // file version: EV_CURRENT
-    view.os_abi_mut().write(0); // OS/ABI identification: System V
-    view.abi_version_mut().write(0); // ABI version: System V third edition
-    view.pad_mut().copy_from_slice(&[0u8; 7]);
-}
+    fn set_ident<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: elf64_ident::View<S>) {
+        view.mag_mut()
+            .copy_from_slice(&[0x7f, 'E' as u8, 'L' as u8, 'F' as u8]);
+        view.class_mut().write(2); // class: ELFCLASS64
+        view.data_mut().write(1); // data encoding: ELFDATA2LSB
+        view.version_mut().write(1); // file version: EV_CURRENT
+        view.os_abi_mut().write(0); // OS/ABI identification: System V
+        view.abi_version_mut().write(0); // ABI version: System V third edition
+        view.pad_mut().copy_from_slice(&[0u8; 7]);
+    }
 
-binary_layout!(elf64_hdr, LittleEndian, {
-    ident: elf64_ident::NestedView,
-    _type: Elf64_Half,
-    machine: Elf64_Half,
-    version: Elf64_Word,
-    entry: Elf64_Addr, // virtual address of entry point
-    phoff: Elf64_Off, // program header
-    shoff: Elf64_Off, // section header
-    flags: Elf64_Word, // processor-specific
-    ehsize: Elf64_Half,
-    phentsize: Elf64_Half,
-    phnum: Elf64_Half, // number of program header entries
-    shentsize: Elf64_Half, // size of section header entry
-    shnum: Elf64_Half, // number of section header entries
-    shstrndx: Elf64_Half, // section name string table index
-});
+    binary_layout!(elf64_hdr, LittleEndian, {
+        ident: elf64_ident::NestedView,
+        _type: Elf64_Half,
+        machine: Elf64_Half,
+        version: Elf64_Word,
+        entry: Elf64_Addr, // virtual address of entry point
+        phoff: Elf64_Off, // program header
+        shoff: Elf64_Off, // section header
+        flags: Elf64_Word, // processor-specific
+        ehsize: Elf64_Half,
+        phentsize: Elf64_Half,
+        phnum: Elf64_Half, // number of program header entries
+        shentsize: Elf64_Half, // size of section header entry
+        shnum: Elf64_Half, // number of section header entries
+        shstrndx: Elf64_Half, // section name string table index
+    });
 
-const PROGRAM_OFFSET: u64 = {
-    // XXX: manually implement unwrap since it isn't stable as a const fn
-    let sz1 = match elf64_hdr::SIZE {
-        Some(s) => s,
-        None => panic!("unsized"),
+    const PROGRAM_OFFSET: u64 = {
+        // XXX: manually implement unwrap since it isn't stable as a const fn
+        let sz1 = match elf64_hdr::SIZE {
+            Some(s) => s,
+            None => panic!("unsized"),
+        };
+        let sz2 = match elf64_phdr::SIZE {
+            Some(s) => s,
+            None => panic!("unsized"),
+        };
+        (sz1 + sz2) as u64
     };
-    let sz2 = match elf64_phdr::SIZE {
-        Some(s) => s,
-        None => panic!("unsized"),
-    };
-    (sz1 + sz2) as u64
-};
 
-pub const VADDR: u64 = 0x400000;
+    const VADDR: u64 = 0x400000;
 
-fn set_elf64_hdr<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: elf64_hdr::View<S>) {
-    set_ident(view.ident_mut());
-    view._type_mut().write(2); // ET_EXEC
-    view.machine_mut().write(62); // EM_X86_64
-    view.version_mut().write(1); // EV_CURRENT
-    view.entry_mut().write(VADDR + PROGRAM_OFFSET);
-    view.phoff_mut().write(elf64_hdr::SIZE.unwrap() as u64);
-    view.flags_mut().write(0); // no processor-specific flags
-    view.ehsize_mut().write(elf64_hdr::SIZE.unwrap() as u16);
-    view.phentsize_mut().write(elf64_phdr::SIZE.unwrap() as u16);
-    view.phnum_mut().write(1);
+    pub fn set_elf64_hdr<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: elf64_hdr::View<S>) {
+        set_ident(view.ident_mut());
+        view._type_mut().write(2); // ET_EXEC
+        view.machine_mut().write(62); // EM_X86_64
+        view.version_mut().write(1); // EV_CURRENT
+        view.entry_mut().write(VADDR + PROGRAM_OFFSET);
+        view.phoff_mut().write(elf64_hdr::SIZE.unwrap() as u64);
+        view.flags_mut().write(0); // no processor-specific flags
+        view.ehsize_mut().write(elf64_hdr::SIZE.unwrap() as u16);
+        view.phentsize_mut().write(elf64_phdr::SIZE.unwrap() as u16);
+        view.phnum_mut().write(1);
+    }
+
+    binary_layout!(elf64_phdr, LittleEndian, {
+        _type: Elf64_Word,
+        flags: Elf64_Word,
+        offset: Elf64_Off,
+        vaddr: Elf64_Addr,
+        paddr: Elf64_Addr,
+        filesz: Elf64_Xword,
+        memsz: Elf64_Xword,
+        align: Elf64_Xword,
+    });
+
+    pub fn set_elf64_phdr<S>(mut view: elf64_phdr::View<S>, program_size: u64)
+    where
+        S: AsRef<[u8]> + AsMut<[u8]>,
+    {
+        view._type_mut().write(1); // PT_LOAD
+        view.flags_mut().write(0x1 | 0x2 | 0x4); // PF_X | PF_W | PF_R
+
+        // location of segment in file
+        let offset = (elf64_hdr::SIZE.unwrap() + elf64_phdr::SIZE.unwrap()) as u64;
+        view.offset_mut().write(offset);
+        // virtual address of segment
+        view.vaddr_mut().write(VADDR + PROGRAM_OFFSET);
+
+        view.filesz_mut().write(program_size);
+        view.memsz_mut().write(program_size);
+        view.align_mut().write(4096);
+    }
+
+    binary_layout!(elf64_file, LittleEndian, {
+        hdr: elf64_hdr::NestedView,
+        phdr: elf64_phdr::NestedView,
+        program: [u8],
+    });
 }
 
-binary_layout!(elf64_phdr, LittleEndian, {
-    _type: Elf64_Word,
-    flags: Elf64_Word,
-    offset: Elf64_Off,
-    vaddr: Elf64_Addr,
-    paddr: Elf64_Addr,
-    filesz: Elf64_Xword,
-    memsz: Elf64_Xword,
-    align: Elf64_Xword,
-});
-
-fn set_elf64_phdr<S>(mut view: elf64_phdr::View<S>, program_size: u64)
-where
-    S: AsRef<[u8]> + AsMut<[u8]>,
-{
-    view._type_mut().write(1); // PT_LOAD
-    view.flags_mut().write(0x1 | 0x2 | 0x4); // PF_X | PF_W | PF_R
-
-    // location of segment in file
-    let offset = (elf64_hdr::SIZE.unwrap() + elf64_phdr::SIZE.unwrap()) as u64;
-    view.offset_mut().write(offset);
-    // virtual address of segment
-    view.vaddr_mut().write(VADDR + PROGRAM_OFFSET);
-
-    view.filesz_mut().write(program_size);
-    view.memsz_mut().write(program_size);
-    view.align_mut().write(4096);
-}
-
-binary_layout!(elf64_file, LittleEndian, {
-    hdr: elf64_hdr::NestedView,
-    phdr: elf64_phdr::NestedView,
-    program: [u8],
-});
+use elf_internal::*;
 
 pub fn shellcode_to_exe(shellcode: &[u8]) -> Vec<u8> {
     let hdr_sz = elf64_hdr::SIZE.unwrap();
