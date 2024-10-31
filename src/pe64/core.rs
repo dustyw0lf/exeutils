ab4ef644a7a1e47462e7ed5b15f302d2f795864e
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-10-31 16:51:46 +0200

    Implement functions for setting PE64 headers

--- src/pe64/core.rs
+++ src/pe64/core.rs
@@ -4,7 +4,7 @@ use super::layout::*;
 /// Sets up the PE64 IMAGE_DOS_HEADER header
 fn set_image_dos_header<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: image_dos_header::View<S>) {
     view.e_magic_mut().write(E_MAGIC);
-    view.e_lfanew_mut().write(0x40);
+    view.e_lfanew_mut().write(0x40); // ?
 }
 
 /// Sets up the IMAGE_NT_HEADERS64 header
@@ -17,13 +17,23 @@ fn set_image_nt_headers64<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: image_nt_heade
 /// Sets up the IMAGE_FILE_HEADER haeder
 fn set_image_file_header<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: image_file_header::View<S>) {
     view.machine_mut().write(IMAGE_FILE_MACHINE_AMD64);
+    view.characteristics_mut()
+        .write(IMAGE_FILE_EXECUTABLE_IMAGE);
 }
 
 /// Sets up the IMAGE_OPTIONAL_HEADER64 haeder
 fn set_image_optional_header64<S: AsRef<[u8]> + AsMut<[u8]>>(
     mut view: image_optional_header64::View<S>,
 ) {
-    todo!()
+    view.magic_mut().write(IMAGE_NT_OPTIONAL_HDR64_MAGIC);
+    view.address_of_entry_point_mut().write(0x140);
+    view.image_base_mut().write(0x400000);
+    view.section_alignment_mut().write(1);
+    view.file_alignment_mut().write(1);
+    view.major_subsystem_version_mut().write(4);
+    view.size_of_image_mut().write(0x160); // ?
+    view.size_of_headers_mut().write(0x140); // ?
+    view.subsystem_mut().write(3); // CLI
 }
 
 #[allow(unused_variables)]
