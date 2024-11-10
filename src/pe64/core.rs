d46341e46dfc9de62ecd585f64a0024f7b80fd18
dustyw0lf <dustyw0lf@users.noreply.github.com>
2024-11-10 15:58:07 +0200

    Refactor(pe64): Remove duplicate function definitions

--- src/pe64/core.rs
+++ src/pe64/core.rs
@@ -178,31 +178,6 @@ fn set_image_section_header<S: AsRef<[u8]> + AsMut<[u8]>>(
     );
 }
 
-/// Sets up the IMAGE_FILE_HEADER haeder
-fn set_image_file_header<S: AsRef<[u8]> + AsMut<[u8]>>(mut view: image_file_header::View<S>) {
-    view.machine_mut().write(IMAGE_FILE_MACHINE_AMD64);
-    view.characteristics_mut()
-        .write(IMAGE_FILE_EXECUTABLE_IMAGE);
-}
-
-/// Sets up the IMAGE_OPTIONAL_HEADER64 header
-fn set_image_optional_header64<S: AsRef<[u8]> + AsMut<[u8]>>(
-    mut view: image_optional_header64::View<S>,
-    image_size: DWORD,
-    headers_size: DWORD,
-) {
-    view.magic_mut().write(IMAGE_NT_OPTIONAL_HDR64_MAGIC);
-    view.address_of_entry_point_mut().write(headers_size); // RVA
-    view.image_base_mut().write(IMAGE_BASE);
-    view.section_alignment_mut().write(1);
-    view.file_alignment_mut().write(1);
-    view.major_subsystem_version_mut()
-        .write(MAJOR_SUBSYSTEM_VERSION);
-    view.size_of_image_mut().write(image_size);
-    view.size_of_headers_mut().write(headers_size);
-    view.subsystem_mut().write(IMAGE_SUBSYSTEM_WINDOWS_CUI);
-}
-
 pub fn shellcode_to_exe(shellcode: &[u8]) -> Vec<u8> {
     let dos_header_size = image_dos_header::SIZE.unwrap();
     let nt_headers64_size = image_nt_headers64::SIZE.unwrap();
